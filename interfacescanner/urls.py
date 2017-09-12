45f94b5b26fc00f8193a503ec42261e52bdd71e5
e <494518881@qq.com>
2017-09-13 00:45:32 +0800

    'second'

--- interfacescanner/urls.py
+++ interfacescanner/urls.py
@@ -1,10 +1,14 @@
 #coding:utf-8
 from django.conf.urls import url
 from .views import IndexView, LoginView
+from .import views
 
 
 app_name = "interfacescanner"
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name='index'),
-    url(r'login/', LoginView.as_view(), name='login'),
+    url(r'login/$', LoginView.as_view(), name='login'),
+    url(r'list/$', views.List, name='list'),
+    url(r'list/verify/$', views.Verify, name='verify'),
+
 ]
--- interfacescanner/views.py
+++ interfacescanner/views.py
@@ -2,6 +2,9 @@
 from django.views.generic.base import View
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+
+from django.http import  HttpResponse,HttpResponseRedirect
 from django.contrib.auth.models import User
 from .forms import LoginForm
 from .models import InterFace
@@ -11,14 +14,6 @@ from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
 
 class IndexView(View):
     def get(self, request):
-        return render(request, 'login.html')
-
-
-class LoginView(View):
-    def get(self, request):
-        return render(request, 'login.html')
-
-    def post(self, request):
         interface_list = InterFace.objects.all()
 
         try:
@@ -26,10 +21,18 @@ class LoginView(View):
         except PageNotAnInteger:
             page = 1
 
-        p = Paginator(interface_list, 1, request=request)  #注意这里一个数字5，表示每页显示的数量
+        p = Paginator(interface_list, 2, request=request)  # 这里的数字，表示每页显示的数量
 
         interfaces = p.page(page)
 
+        return render(request, 'index.html', {"interface_list": interfaces})
+
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'login.html')
+
+    def post(self, request):
         login_form = LoginForm(request.POST)
         if login_form.is_valid():
             user_name = request.POST.get("username", "")
@@ -38,13 +41,33 @@ class LoginView(View):
             user = authenticate(username=user_name, password=pass_word)
             if user is not None:
                 login(request, user)
-                return render(request, 'list2.html', {"interface_list": interfaces})
+                response = HttpResponseRedirect('/list/')  # 登录成功跳转list
+                request.session['username'] = user_name  # 将 session 信息写到服务器
+                return response
             else:
                 return render(request, 'login.html', {"msg":"用户名或者密码错误"})
         else:
             return render(request, 'login.html', {"msg":"请填写账号或者密码"})
 
 
+@login_required  # 这个装饰器只能装饰def不能是class
+def List(request):
+
+    username = request.session.get('username', '')
+    interface_list = InterFace.objects.all()
+    try:
+        page = request.GET.get('page', 1)
+    except PageNotAnInteger:
+        page = 1
+    p = Paginator(interface_list, 2, request=request)  # 这里的数字，表示每页显示的数量
+    interfaces = p.page(page)
+
+    return render(request, 'list.html', {"user": username, "interface_list": interfaces})
+
+
+@login_required
+def Verify(request):
+    interfaces_id = request.POST.get()
 
 
 
